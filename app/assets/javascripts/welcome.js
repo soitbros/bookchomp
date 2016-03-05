7eef67a653d4e7e47be241e2f97f06d3d9bc4f36
Clint Osterholz <clintosterholz@gmail.com>
2016-03-05 18:53:38 -0500

    adds a couple CSS tweaks

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -2,7 +2,6 @@ var app = angular.module('Chomp', []);
 
 app.controller('PostsController', ['$scope', '$http', function($scope, $http){
   $http.get('/posts').then(function(response){
-    console.log(response.data);
     var data = response.data;
     $scope.posts = data.posts;
   });
